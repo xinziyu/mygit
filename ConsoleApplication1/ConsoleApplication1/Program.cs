15e7fba92c218022d831bfe5b81d250791dd4524
xuyang <xinziyujob@163.com>
2015-12-03 22:06:24 +0800

    master

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            method1();
+        }
+
+        private static void method1()
+        {
+            Console.WriteLine("123");
+        }
+    }
+}
